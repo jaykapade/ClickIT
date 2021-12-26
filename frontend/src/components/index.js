4aba1dbf0886df2a1597bf0e3de0e42967788d44
jazzdestructor <jazzdestructor@gmail.com>
2021-12-26 23:16:05 +0530

    Added Navbar

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,4 +5,4 @@ export { default as CreatePin } from "./CreatePin";
 export { default as Feed } from "./Feed";
 export { default as Navbar } from "./Navbar";
 export { default as PinDetail } from "./PinDetail";
-export { default as Search } from "./Login";
+export { default as Search } from "./Search";

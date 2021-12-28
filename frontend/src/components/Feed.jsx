0bf337e851975278f2283add1e4933e9f0bed977
jazzdestructor <jazzdestructor@gmail.com>
2021-12-28 23:02:11 +0530

    Added Pins to Feed and made the download functionality

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -3,13 +3,33 @@ import { useParams } from "react-router-dom";
 
 import { client } from "../client";
 import { MasonryLayout, Spinner } from "../components";
+import { feedQuery, searchQuery } from "../utils/data";
 
 const Feed = () => {
   const [loading, setLoading] = useState(false);
-
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        // console.log(data);
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        // console.log(data);
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
   if (loading)
     return <Spinner message="We are adding new ideas to your feed!!" />;
-  return <div></div>;
+
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,11 +21,11 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
       </div>
       <div className="flex gap-3">
         <Link to={`user-profile/${user?._id}`} className="hidden md:block">
-          <img src={user.image} alt="user" className="w-14 h-12 rounded-full" />
+          <img src={user.image} alt="user" className="w-14 h-12 rounded-lg" />
         </Link>
         <Link
           to={`create-pin`}
-          className="bg-gray-200 text-gray-600 rounded-full w-14 h-12 flex justify-center items-center"
+          className="bg-gray-200 text-gray-600 rounded-lg w-14 h-12 flex justify-center items-center"
         >
           <IoIosAdd fontSize={21} />
         </Link>
